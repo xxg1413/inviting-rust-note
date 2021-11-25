fc9d561fb90378702de7d62bb252fc8d039807df
pxiaoer <xxg1413@gmail.com>
2021-11-25 11:32:23 +0800

    Add 14

---
+++ tutorial/14/clone_trait.rs
@@ -0,0 +1,27 @@
+
+#[derive(Clone, Debug)]
+struct Developer {
+  name: String,
+  age: u8,
+  lang: Language
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+enum Language {
+  Rust,
+  TypeScript,
+  Elixir,
+  Haskell
+}
+
+fn main() {
+    let dev = Developer {
+        name: "Tyr".to_string(),
+        age: 18,
+        lang: Language::Rust
+    };
+    let dev1 = dev.clone();
+    println!("dev: {:?}, addr of dev name: {:p}", dev, dev.name.as_str());
+    println!("dev1: {:?}, addr of dev1 name: {:p}", dev1, dev1.name.as_str())
+}
